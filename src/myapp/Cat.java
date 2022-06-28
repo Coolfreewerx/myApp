ed3223cad19333def8269bd2c365cb972b4bad4f
Tam Nattaponr <nattapon.mee@ku.th>
2022-06-29 01:09:46 +0700

    Update Cat and Dog

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,5 +3,6 @@ package myapp;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Cat meow meow");
+        System.out.println("Cat 2 meow meow");
     }
 }
