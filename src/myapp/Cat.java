c6eeefefe5647c41efa2d2a340a164b99dfb48f0
Tam Nattaponr <nattapon.mee@ku.th>
2022-06-29 01:18:12 +0700

    bring back src/myapp/Cat.java from commit faf3636

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,6 +3,5 @@ package myapp;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Cat meow meow");
-        System.out.println("Cat 2 meow meow");
     }
 }
